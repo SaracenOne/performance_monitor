e8a0799e3b8efe2ef5bfac526066406fa0381ccd
Saracen <SaracenOne@gmail.com>
2020-02-05 21:35:11 +0000

    Add typing

--- performance_monitor_plugin.gd
+++ performance_monitor_plugin.gd
@@ -1,11 +1,11 @@
 extends EditorPlugin
 tool
 
-func get_name(): 
+func get_name() -> String:
 	return "PerformanceMonitor"
 
-func _enter_tree():
+func _enter_tree() -> void:
 	add_autoload_singleton("PerformanceMonitor", "res://addons/performance_monitor/performance_monitor.gd")
 
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_autoload_singleton("PerformanceMonitor")
