772fd0e28ad73bbee5ec15d7be93c9d9f38649eb
Saracen <SaracenOne@gmail.com>
2020-10-04 16:06:46 +0100

    Add missing typing

--- performance_monitor.gd
+++ performance_monitor.gd
@@ -10,13 +10,13 @@ func toggle_performance_monitor() -> void:
 	if performance_monitor_gui:
 		performance_monitor_gui.show_performance_monitor(!performance_monitor_gui.should_show_performance_monitor)
 
-func _input(p_event):
+func _input(p_event: InputEvent) -> void:
 	if ! Engine.is_editor_hint():
 		if p_event.is_action("toggle_performance_monitor"):
 			if p_event.is_pressed():
 				toggle_performance_monitor()
 
-func _ready():
+func _ready() -> void:
 	if ! Engine.is_editor_hint():
 		canvas_layer = CanvasLayer.new()
 		canvas_layer.set_name("PerformanceMonitorLayer")
